d1f1a457009ccfd281e1b501ee105038cb1d7990
OdiljonMirzaxamdamov <odiojon1994@gmail.com>
2024-03-17 09:42:30 +0500

    лекция-6, датабильдинг

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -1,55 +1,44 @@
+/* eslint-disable valid-jsdoc */
+
 import LoadMoreButtonComponent from "../components/load-more-button.js";
-import NoTasksComponent from "../components/no-task.js";
+import NoTasksComponent from "../components/no-tasks.js";
 import SortComponent, {SortType} from "../components/sort.js";
-import TaskComponent from "../components/task.js";
-import TaskEditComponent from "../components/task-edit.js";
+import TaskController from "./task";
 import TasksComponent from "../components/tasks.js";
-import {remove, render, replace, RenderPosition} from "../utils/render.js";
+import {remove, render, RenderPosition} from "../utils/render.js";
 
 
+/** Количество задач, которое по-умолчанию будет отображаться */
 const SHOWING_TASKS_COUNT_ON_START = 8;
+/** Количество задач, которое будет подгружаться по кнопке LoadMore */
 const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
-const renderTask = (taskListElement, task) => {
-  const replaceTaskToEdit = () => {
-    replace(taskEditComponent, taskComponent);
-  };
 
-  const replaceEditToTask = () => {
-    replace(taskComponent, taskEditComponent);
-  };
+/**
+ * Функция, которая рендерит задачи. Создает массив контроллеров всех показанных задач
+ * @param {Element} taskListElement Элемент, куда будут добавляться задачи
+ * @param {Array} tasks Массив задач, которые нужно отрисовать
+ * @param {Function} onDataChange Метод, который будет вызван, при изменении данных
+ * @param {Function} onViewChange Метод, который будет возвращать карточку в дефолтное состояние
+ */
+const renderTasks = (taskListElement, tasks, onDataChange, onViewChange) => {
+  return tasks.map((task) => {
+    const taskController = new TaskController(taskListElement, onDataChange, onViewChange);
 
-  const onEscKeyDown = (evt) => {
-    const isEscKey = evt.key === `Esc` || evt.key === `Escape`;
+    taskController.render(task);
 
-    if (isEscKey) {
-      replaceEditToTask();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  const taskComponent = new TaskComponent(task);
-  taskComponent.setEditButtonClickHandler(() => {
-    replaceTaskToEdit();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  });
-
-  const taskEditComponent = new TaskEditComponent(task);
-  taskEditComponent.setSubmitHandler((evt) => {
-    evt.preventDefault();
-    replaceEditToTask();
-    document.removeEventListener(`keydown`, onEscKeyDown);
-  });
-
-  render(taskListElement, taskComponent, RenderPosition.BEFOREEND);
-};
-
-const renderTasks = (taskListElement, tasks) => {
-  tasks.forEach((task) => {
-    renderTask(taskListElement, task);
+    return taskController;
   });
 };
 
+
+/**
+ * Функция, которая возвращает отсортированный массив задач
+ * @param {Array} tasks Массив задач
+ * @param {*} sortType Тип сортировки
+ * @param {Number} from Индекс элемента, начиная с которого нужно возвращать задачи
+ * @param {Number} to Индекс элемента, до которого нужно возвращать задачи
+ */
 const getSortedTasks = (tasks, sortType, from, to) => {
   let sortedTasks = [];
   const showingTasks = tasks.slice();
@@ -69,89 +58,148 @@ const getSortedTasks = (tasks, sortType, from, to) => {
   return sortedTasks.slice(from, to);
 };
 
+
+/** Контроллер: Доска задач */
 export default class BoardController {
+  /**
+   * Конструктор контроллера "Доска задач"
+   * @param {*} container Компонент, внутри которого будет доска
+   */
   constructor(container) {
+    /** Свойство контроллера: Компонент, внутри которого будет текущая доска */
     this._container = container;
+    /** Свойство контроллера: Массив всех задач */
+    this._tasks = [];
+    /** Свойство контроллера: Массив контроллеров отображенных задач (наблюдателей) */
+    this._showedTaskControllers = [];
+    /** Свойство контроллера: Количество отображаемых задач */
+    this._showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
+    /** Свойство контроллера: Компонент доски без задач */
     this._noTasksComponent = new NoTasksComponent();
+    /** Свойство контроллера: Компонент доски - Сортировка */
     this._sortComponent = new SortComponent();
+    /** Свойство контроллера: Компонент доски - Блок со всеми задачами */
     this._tasksComponent = new TasksComponent();
+    /** Свойство контроллера: Компонент доски LoadMore */
     this._loadMoreButtonComponent = new LoadMoreButtonComponent();
+
+    /** Свойство контроллера: метод изменения данных и перерисовки компонентов в контексте текущего контроллера доски */
+    this._onDataChange = this._onDataChange.bind(this);
+    /** Свойство контроллера: метод изменения типа сортировки в контексте текущего контроллера доски */
+    this._onSortTypeChange = this._onSortTypeChange.bind(this);
+    /** Свойство контроллера: метод, который уведомляет все контроллеры задач, что они должны вернуться в дефолтный режим в контексте текущего контроллера доски */
+    this._onViewChange = this._onViewChange.bind(this);
+
+    /** Добавление обработчика на изменение типа сортировки */
+    this._sortComponent.setSortTypeChangeHandler(this._onSortTypeChange);
   }
 
 
+  /**
+   * Метод для рендеринга доски
+   * @param {Array} tasks Массив задач
+   */
   render(tasks) {
-    // Функция для отображения кнопки "Показать еще"
-    const renderLoadMoreButton = () => {
-      // Проверяем, не превышено ли количество отображаемых задач
-      if (showingTasksCount >= tasks.length) {
-        return;
-      }
+    this._tasks = tasks;
 
-      // Отображаем кнопку "Показать еще" перед списком задач
-      render(container, this._loadMoreButtonComponent, RenderPosition.BEFOREEND);
-
-      // Устанавливаем обработчик клика на кнопку "Показать еще"
-      this._loadMoreButtonComponent.setClickHandler(() => {
-        // Запоминаем количество отображенных задач до клика
-        const prevTasksCount = showingTasksCount;
-        // Увеличиваем количество отображаемых задач
-        showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
-
-        // Получаем отсортированные задачи с учетом типа сортировки
-        const sortedTasks = getSortedTasks(tasks, this._sortComponent.getSortType(), prevTasksCount, showingTasksCount);
-
-        // Отображаем задачи после клика
-        renderTasks(taskListElement, sortedTasks);
-      });
-
-      // Если отображены все задачи, удаляем кнопку "Показать еще"
-      if (showingTasksCount >= tasks.length) {
-        remove(this._loadMoreButtonComponent);
-      }
-    };
-
-    // Получаем контейнер для отображения компонентов
+    /** Элемент, внутри которого будет рендериться доска */
     const container = this._container.getElement();
+    /** Флаг для проверки того, что есть НЕ архивные задачи */
+    const isAllTasksArchived = this._tasks.every((task) => task.isArchive);
 
-    // Проверяем, все ли задачи архивированы
-    const isAllTasksArchived = tasks.every((task) => task.isArchive);
-    // Если все задачи архивированы, отображаем сообщение об отсутствии задач
     if (isAllTasksArchived) {
       render(container, this._noTasksComponent, RenderPosition.BEFOREEND);
       return;
     }
 
-    // Отображаем компонент сортировки
     render(container, this._sortComponent, RenderPosition.BEFOREEND);
-    // Отображаем компонент списка задач
     render(container, this._tasksComponent, RenderPosition.BEFOREEND);
-    // Получаем элемент списка задач
+
+    /** Элемент, внутри которого будут рендериться задачи */
+    const taskListElement = this._tasksComponent.getElement();
+    const newTasks = renderTasks(taskListElement, this._tasks.slice(0, this._showingTasksCount), this._onDataChange, this._onViewChange);
+    this._showedTaskControllers = this._showedTaskControllers.concat(newTasks);
+
+    this._renderLoadMoreButton();
+  }
+
+
+  /** Приватный метод, для отрисовки кнопки LoadMore*/
+  _renderLoadMoreButton() {
+    if (this._showingTasksCount >= this._tasks.length) {
+      return;
+    }
+
+    /** Компронент, внутри которого будет кнопка LoadMore */
+    const container = this._container.getElement();
+    render(container, this._loadMoreButtonComponent, RenderPosition.BEFOREEND);
+
+    /** Обработчик события клика по кнопке LoadMore */
+    this._loadMoreButtonComponent.setClickHandler(() => {
+      /** Количество показанных задач */
+      const prevTasksCount = this._showingTasksCount;
+      /** Элемент доски со всеми задачами */
+      const taskListElement = this._tasksComponent.getElement();
+      this._showingTasksCount = this._showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
+
+      /** Массив отсортированных задач, которые появятся при клике на LoadMore */
+      const sortedTasks = getSortedTasks(this._tasks, this._sortComponent.getSortType(), prevTasksCount, this._showingTasksCount);
+      /** Массив новых отрендеренных сортированных задач */
+      const newTasks = renderTasks(taskListElement, sortedTasks, this._onDataChange, this._onViewChange);
+
+      this._showedTaskControllers = this._showedTaskControllers.concat(newTasks);
+
+      if (this._showingTasksCount >= this._tasks.length) {
+        remove(this._loadMoreButtonComponent);
+      }
+    });
+  }
+
+
+  /**
+   * Приватный метод, который изменяет данные и перерисовывает компонент
+   * @param {*} taskController Контроллер задачи
+   * @param {*} oldData Старые данные
+   * @param {*} newData Новые данные
+   */
+  _onDataChange(taskController, oldData, newData) {
+    /** Индекс задачи, в которой произошли изменения */
+    const index = this._tasks.findIndex((it) => it === oldData);
+
+    if (index === -1) {
+      return;
+    }
+
+    this._tasks = [].concat(this._tasks.slice(0, index), newData, this._tasks.slice(index + 1));
+
+    taskController.render(this._tasks[index]);
+  }
+
+
+  /** Приватный метод, который уведомляет все контроллеры задач, что они должны вернуться в дефолтный режим  */
+  _onViewChange() {
+    this._showedTaskControllers.forEach((it) => it.setDefaultView());
+  }
+
+
+  /**
+   * Приватный метод, который перерисовывает задачи при изменении типа сортировки
+   * @param {*} sortType Тип сортировки
+   */
+  _onSortTypeChange(sortType) {
+    this._showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
+
+    /** Массив отсортированных задач */
+    const sortedTasks = getSortedTasks(this._tasks, sortType, 0, this._showingTasksCount);
+    /** Элемент доски со всеми задачами */
     const taskListElement = this._tasksComponent.getElement();
 
-    // Начальное количество отображаемых задач
-    let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
+    taskListElement.innerHTML = ``;
 
-    // Отображаем начальный список задач
-    renderTasks(taskListElement, tasks.slice(0, showingTasksCount));
-    // Отображаем кнопку "Показать еще"
-    renderLoadMoreButton();
+    /** Массив новых отрендеренных сортированных задач */
+    const newTasks = renderTasks(taskListElement, sortedTasks, this._onDataChange, this._onViewChange);
+    this._showedTaskControllers = newTasks;
 
-    // Устанавливаем обработчик изменения типа сортировки
-    this._sortComponent.setSortTypeChangeHandler((sortType) => {
-      // Сбрасываем количество отображаемых задач до стандартного значения
-      showingTasksCount = SHOWING_TASKS_COUNT_BY_BUTTON;
-
-      // Получаем отсортированные задачи с учетом нового типа сортировки
-      const sortedTasks = getSortedTasks(tasks, sortType, 0, showingTasksCount);
-
-      // Очищаем список задач
-      taskListElement.innerHTML = ``;
-
-      // Отображаем отсортированные задачи
-      renderTasks(taskListElement, sortedTasks);
-      // Отображаем кнопку "Показать еще"
-      renderLoadMoreButton();
-    });
-
+    this._renderLoadMoreButton();
   }
 }
